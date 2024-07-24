0eed0f2c244602ba3abdae7abcfa3aeb593f21ad
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2024-07-24 21:20:25 +0530

    Add files via upload

---
+++ Day-3/print a string and extract charecters.py
@@ -0,0 +1,9 @@
+str=input("Enter the string:")
+print(str)                          # printing complete string
+print(str[0])                       # printing 0th (first) elements of str1
+print(str[1],str[2])                # printing second & third elements
+print(str[2:5])                     # printing elements from 2nd to 5th index
+print(str[1:])                      # printing all elements from 1st index
+print(str *3)                       # printing string three times
+print(str[:-1])                     #print string except last letter
+print(str[:-2])                     #print string except last two letters
