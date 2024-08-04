ebcf6c52a92bf3f878519ca4e37b4af5cd2bef0d
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2024-08-05 01:43:01 +0530

    Add files via upload

---
+++ Day-5/converting characters into list.py
@@ -0,0 +1,14 @@
+list1 = ['H', 'e', 'l', 'l', 'o']
+
+# printing characters list and its type
+print("list1: ", list1)
+print("type(list1): ", type(list1))
+print()
+
+# converting character list to the string
+str1 = ""
+str1 = str1.join(list1)
+
+# print the string and its type
+print("str1: ", str1)
+print("type(str1): ", type(str1))
