c2f50b128ba997d249007d15b23feb8c6cc502fb
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2024-08-24 14:10:45 +0530

    Add files via upload

---
+++ Day-8/add numbers using objects.py
@@ -0,0 +1,10 @@
+class add:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def res(self):
+        self.c= self.num1 +self.num2
+        print(self.c)
+    
+n1=add(2,4)
+n1.res()
